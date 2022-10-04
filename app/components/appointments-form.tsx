7b88557c451e72b9af7be4c8dcf3652dc2e01467
Thiago Cruz Santos <thiagocruzsantos@gmail.com>
2022-10-04 11:06:25 -0300

    added loading indicators and fixed bug on revalidate

--- app/components/appointments-form.tsx
+++ app/components/appointments-form.tsx
@@ -1,4 +1,4 @@
-import { Form, useActionData } from "@remix-run/react";
+import { Form, useActionData, useTransition } from "@remix-run/react";
 import { type Appointment } from "~/models/appointments";
 
 export default function AppointmentForm({
@@ -7,47 +7,52 @@ export default function AppointmentForm({
   appointment?: Appointment;
 }) {
   const actionData = useActionData();
+  const transition = useTransition();
   return (
     <Form method="post">
       <h2>Create new appointment:</h2>
-      <div>
-        <label htmlFor="title">Title</label>{" "}
-        <input
-          id="title"
-          name="title"
-          type="text"
-          placeholder="Title"
-          defaultValue={actionData?.values.title}
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor="start">Start</label>{" "}
-        <input
-          type="datetime-local"
-          id="start"
-          name="start"
-          defaultValue={actionData?.values.start}
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor="finish">Finish</label>{" "}
-        <input
-          type="datetime-local"
-          id="finish"
-          name="finish"
-          defaultValue={actionData?.values.finish}
-          required
-        />
-      </div>
-      {actionData?.errors.start ? (
-        <p style={{ color: "red" }}>{actionData.errors.start}</p>
-      ) : null}
-      {actionData?.errors.finish ? (
-        <p style={{ color: "red" }}>{actionData.errors.finish}</p>
-      ) : null}
-      <button type="submit">Submit</button>
+      <fieldset disabled={transition.state === "submitting"}>
+        <div>
+          <label htmlFor="title">Title</label>{" "}
+          <input
+            id="title"
+            name="title"
+            type="text"
+            placeholder="Title"
+            defaultValue={actionData?.values.title}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="start">Start</label>{" "}
+          <input
+            type="datetime-local"
+            id="start"
+            name="start"
+            defaultValue={actionData?.values.start}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="finish">Finish</label>{" "}
+          <input
+            type="datetime-local"
+            id="finish"
+            name="finish"
+            defaultValue={actionData?.values.finish}
+            required
+          />
+        </div>
+        {actionData?.errors.start ? (
+          <p style={{ color: "red" }}>{actionData.errors.start}</p>
+        ) : null}
+        {actionData?.errors.finish ? (
+          <p style={{ color: "red" }}>{actionData.errors.finish}</p>
+        ) : null}
+        <button type="submit">
+          {transition.state === "submitting" ? "Loading..." : "Create"}
+        </button>
+      </fieldset>
     </Form>
   );
 }
--- app/lib/use-revalidate.ts
+++ app/lib/use-revalidate.ts
@@ -1,12 +1,13 @@
-import { useNavigate } from "@remix-run/react";
+import { useLocation, useNavigate } from "@remix-run/react";
 import { useCallback } from "react";
 
 export function useRevalidate() {
   let navigate = useNavigate();
+  let location = useLocation();
   return useCallback(
     function revalidate() {
-      navigate(".", { replace: true });
+      navigate("." + location.search, { replace: true });
     },
-    [navigate]
+    [navigate, location]
   );
 }
